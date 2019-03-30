0f12b886b8ca67913f7afb3d886d990fa0cbffe7
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2019-03-30 11:46:36 +0100

    banner done

--- basic-frontend-auth-master/src/components/Banner.js
+++ basic-frontend-auth-master/src/components/Banner.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import tripService from "../lib/api-service";
+import "./banner.css";
+import moment from 'moment';
+
+
 
 
 export default class Banner extends Component {
@@ -24,14 +28,23 @@ export default class Banner extends Component {
   };
 
   render() {
-    const { city } = this.state.stopOver;
-
+    // const { city } = this.state.stopOver;
+    let time = moment(new Date()).format('h:mm');
     return (
-      <div>
-        <h1>Banner</h1>
-        <h2>{city}</h2>
-        <img src="https://www.indiaretailforum.in/wp-content/uploads/2018/01/mall.jpg" alt="Airport img" />
-
+      <div className="banner">
+        <div>
+          <h1>Hong Kong Airport</h1>
+          {/* <h2>{city} Airport</h2> */}
+          <img src="/honkong.jpg" alt="Airport img" />
+          <div className="next-fligth-time">
+            <p>Current time</p>
+            <h3 >{time}</h3>
+          </div>
+          <div className="current-time">
+            <p>Next departure</p>
+            <h3>15:55</h3>
+          </div>
+        </div>  
       </div>
     )
   }
